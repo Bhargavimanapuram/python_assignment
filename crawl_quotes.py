53fe0d271fdebe51acd09e3a46d362055642a65c
Bhargavimanapuram <bhargavimanapuram@gmail.com>
2022-07-31 15:55:22 +0000

    add list of authors

--- crawl_quotes.py
+++ crawl_quotes.py
@@ -35,16 +35,49 @@ def get_list_of_quotes(data):
         list_of_quotes.append(quote_dict)
     return list_of_sub_quotes
 
+def get_author_details(author_data,author_url):
+    soup = BeautifulSoup(author_data, 'html.parser')
+    author_page_content = soup.find('div',attrs={'class':'author-details'})
+    author_name_element = author_page_content.find('h3',attrs={'class':'author-title'})
+    author_name = author_name_element.getText()
+    born_details_elements = author_page_content.find_all('span')
+    born_details = born_details_elements[0].getText() +" "+ born_details_elements[1].getText()
+    author_details={
+        'name':author_name,
+        'born':born_details,
+        'reference':author_url
+    }
+    return author_details
+
+
+def get_list_of_authors(data):
+    soup = BeautifulSoup(data, 'html.parser')
+    list_of_all_authors_containers = soup.find_all('div', attrs={'class': 'quote'})
+    list_of_sub_authors = []
+    for each_quote in list_of_all_authors_containers:
+        quote_elements = each_quote.find_all('a')
+        author_href = quote_elements[0].get('href')
+        author_url = "http://quotes.toscrape.com" + author_href + "/"
+        author_data = urllib.request.urlopen(author_url)
+        author_details = get_author_details(author_data,author_url)
+        list_of_sub_authors.append(author_details)
+    return list_of_sub_authors
+
+
+
 page = 1
 list_of_quotes = []
+list_of_authors = []
 while page <= 10:
     try:
         url = "http://quotes.toscrape.com/page/" + str(page) + "/"
         data = urllib.request.urlopen(url)
         list_of_sub_quotes = get_list_of_quotes(data)
+        list_of_sub_authors = get_list_of_authors(data)
         list_of_quotes.extend(list_of_sub_quotes)
+        list_of_authors.extend(list_of_sub_authors)
         page += 1
     except:
         break
 
-
+print(len(list_of_authors))
