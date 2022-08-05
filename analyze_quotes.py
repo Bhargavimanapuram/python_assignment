879685537b67fbd7e56efb15b1978f4dab12c321
Bhargavimanapuram <bhargavimanapuram@gmail.com>
2022-08-05 09:59:19 +0000

    final crawl quotes file

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -1,19 +1,17 @@
 import sqlite3
-
 def get_sqlite_connection():
     try:
         connection_obj = sqlite3.connect('quotes.db')
         return connection_obj
-
     except sqlite3.Error as error:
         print(error)
 
 def get_total_no_of_quotations(connection_obj):
     try:
         cursor = connection_obj.cursor()
-        select_query = "SELECT COUNT(id) FROM quote;"
+        select_query = "SELECT COUNT(id) AS total_no_of_quotations FROM quote;"
         cursor.execute(select_query)
-        no_of_quotations = cursor.fetchone()
+        no_of_quotations, = cursor.fetchone()
         return no_of_quotations
     except sqlite3.Error as error:
         print(error)
@@ -23,20 +21,30 @@ def get_no_of_quotations_by_author(connection_obj,author_name):
         cursor = connection_obj.cursor()
         select_query = "SELECT COUNT(id) AS no_of_quotations FROM quote WHERE author_name = ?;"
         cursor.execute(select_query, (author_name,))
-        no_of_quotations_by_author= cursor.fetchone()
+        no_of_quotations_by_author,= cursor.fetchone()
         cursor.close()
         return no_of_quotations_by_author
     except sqlite3.Error as error:
         print(error)
-            
-def get_min_max_avg_of_tags(connection_obj):
+
+def create_view(connection_obj):
     try:
         cursor = connection_obj.cursor()
-        select_query = "SELECT max(no_of_tags), min(no_of_tags),avg(no_of_tags) FROM quote;"
-        cursor.execute(select_query)
-        min_max_avg_of_tags = cursor.fetchone()
+        view_query = "CREATE VIEW no_of_tags_per_quote AS SELECT COUNT(*) AS no_of_tags,quote_id FROM quote_tags GROUP BY quote_id"
+        cursor.execute(view_query)
         cursor.close()
-        return min_max_avg_of_tags
+    except sqlite3.Error as error:
+        print(error)
+
+
+def get_minimum_maximum_average_no_of_tags(connection_obj):
+    try:
+        cursor = connection_obj.cursor()
+        select_query = "SELECT MAX(no_of_tags) AS maximum_no_of_tags, MIN(no_of_tags) As minimum_no_of_tags ,AVG(no_of_tags) AS average_no_of_tags FROM no_of_tags_per_quote"
+        cursor.execute(select_query)
+        maximum,minimum,average = cursor.fetchone()
+        cursor.close()
+        return maximum,minimum,average
     except sqlite3.Error as error:
         print(error)
 
@@ -44,7 +52,7 @@ def get_min_max_avg_of_tags(connection_obj):
 def get_top_n_authors(connection_obj,n):
     try:
         cursor = connection_obj.cursor()
-        select_query = "SELECT author_name,count(id) AS no_of_quotations FROM quote GROUP BY author_name ORDER BY no_of_quotations DESC LIMIT ?;"
+        select_query = "SELECT author_name,count(id) AS no_of_quotations FROM quote GROUP BY author_id ORDER BY no_of_quotations DESC LIMIT ?;"
         cursor.execute(select_query, (n,))
         top_n_authors= cursor.fetchall()
         cursor.close()
@@ -52,9 +60,12 @@ def get_top_n_authors(connection_obj,n):
     except sqlite3.Error as error:
         print(error)
 
-
-connection_obj = get_sqlite_connection()
-no_of_quotations = get_total_no_of_quotations(connection_obj)
-no_of_quotations_by_author = get_no_of_quotations_by_author(connection_obj,"Albert Einstein")
-min_max_avg_of_tags = get_min_max_avg_of_tags(connection_obj)
-top_n_authors = get_top_n_authors(connection_obj,5)
+def calling_all_functions():
+    connection_obj = get_sqlite_connection()
+    no_of_quotations = get_total_no_of_quotations(connection_obj)
+    no_of_quotations_by_author = get_no_of_quotations_by_author(connection_obj,"Albert Einstein")
+    create_view(connection_obj)
+    min_max_avg_of_tags = get_minimum_maximum_average_no_of_tags(connection_obj)
+    top_n_authors = get_top_n_authors(connection_obj,5)
+  
+calling_all_functions()
--- store_quotes.py
+++ store_quotes.py
@@ -1,47 +1,117 @@
 import sqlite3
 import json
+import uuid
 
-connection = sqlite3.connect('quotes.db')
-cursor = connection.cursor()
+def get_sqlite_connection():
+    try:
+        connection = sqlite3.connect('quotes.db')
+        cursor = connection.cursor()
+        return cursor
+    except sqlite3.Error as error:
+        print(error)
 
-cursor.execute("DROP TABLE IF EXISTS quote")
+def enable_foreign_keys(cursor):
+    cursor.execute("PRAGMA foreign_keys = ON")
 
-cursor.execute("""CREATE TABLE quote
-(
-    id INT NOT NULL PRIMARY KEY,
-    quote TEXT,
-    author_name VARCHAR(200),
-    no_of_tags INT
-);""")
+def create_author_table(cursor):
+    cursor.execute("DROP TABLE IF EXISTS author")
+    cursor.execute("""CREATE TABLE author
+    (
+        id INT NOT NULL PRIMARY KEY,
+        author_name VARCHAR(200),
+        author_born_details TEXT
+    );""")
 
-print("Table quote is ready")
+def create_quote_table(cursor):
+    cursor.execute("DROP TABLE IF EXISTS quote")
+    cursor.execute("""CREATE TABLE quote
+    (
+        id INT NOT NULL PRIMARY KEY,
+        quote TEXT,
+        author_name VARCHAR(200),
+        author_id INT,
+        FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE CASCADE
+    );""")
 
-cursor.execute("DROP TABLE IF EXISTS author_details")
+def create_quote_tags_table(cursor):
+    cursor.execute("DROP TABLE IF EXISTS quote_tags")
+    cursor.execute("""CREATE TABLE quote_tags
+    (
+        id VARCHAR(200) NOT NULL PRIMARY KEY,
+        tag_name VARCHAR(200),
+        quote_id INT,
+        author_id INT,
+        FOREIGN KEY(quote_id) REFERENCES quote(id) ON DELETE CASCADE,
+        FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE CASCADE
+    );""")
 
-cursor.execute("""CREATE TABLE author_details
-(
-    id INT NOT NULL PRIMARY KEY,
-    author_name TEXT,
-    author_born_details VARCHAR(200)
-);""")
+def get_json_data():
+    json_data = json.load(open('quotes.json'))
+    quotes = json_data["quotes"]
+    authors = json_data["authors"]
+    return quotes,authors
 
-print("table author_details is ready")
+def insert_values_into_author_table(authors,cursor):
+    for i in range(len(authors)):
+        required_author_details = authors[i]
+        tuple_of_values = (i+1,required_author_details["name"],required_author_details["born"])
+        cursor.execute(""" INSERT INTO author(id,author_name,author_born_details) VALUES (?,?,?)""", tuple_of_values)
 
-json_data = json.load(open('quotes.json'))
+def get_forien_key_value_of_author_id(author_name,cursor):
+    statement = "SELECT id from author WHERE author_name = ?"
+    data = cursor.execute(statement,(author_name,))
+    for row in data:
+        author_id, = row
+        return author_id
 
-quotes = json_data["quotes"]
-authors = json_data["authors"]
+def insert_values_into_quote_table(quotes,cursor):
+    for i in range(len(quotes)):
+        required_quote = quotes[i]
+        id = i+1
+        quote = required_quote["quote"]
+        author_name = required_quote["author"]
+        author_id = get_forien_key_value_of_author_id(author_name,cursor)
+        tuple_of_values = (id,quote,author_name,author_id)
+        cursor.execute(""" INSERT INTO quote(id,quote,author_name,author_id) VALUES (?,?,?,?)""", tuple_of_values)
 
-for i in range(len(quotes)):
-    required_quote = quotes[i]
-    tuple_of_values = (i+1,required_quote["quote"],required_quote["author"],len(required_quote["tags"]))
-    cursor.execute(""" INSERT INTO quote(id,quote,author_name,no_of_tags) VALUES (?,?,?,?)""", tuple_of_values)
+def get_forien_key_values_for_quote_tags_table(quote_id,cursor):
+    statement = "SELECT author_id from quote WHERE id = ?"
+    data = cursor.execute(statement,(quote_id,))
+    for row in data:
+        author_id, = row
+        return author_id
 
-print("Data Inserted in the table: ")
+def insert_values_into_quote_tags_table(quote_id,tags_list_of_quote,author_id,cursor):
+    for tag in tags_list_of_quote:
+        tag_id = str(uuid.uuid4())
+        tag_name = tag
+        tuple_of_values = (tag_id,tag_name,quote_id,author_id)
+        cursor.execute(""" INSERT INTO quote_tags(id,tag_name,quote_id,author_id) VALUES (?,?,?,?)""", tuple_of_values)
 
-for i in range(len(authors)):
-    required_author_details = authors[i]
-    tuple_of_values = (i,required_author_details["name"],required_author_details["born"])
-    cursor.execute(""" INSERT INTO author_details(id,author_name,author_born_details) VALUES (?,?,?)""", tuple_of_values)
+def get_and_insert_list_of_tags_of_each_quote(quotes,cursor):
+    for i in range(len(quotes)):
+        required_quote = quotes[i]
+        quote_id = i+1
+        tags_list_of_quote = required_quote["tags"]
+        author_id = get_forien_key_values_for_quote_tags_table(quote_id,cursor)
+        insert_values_into_quote_tags_table(quote_id,tags_list_of_quote,author_id,cursor)
 
-print("Data Inserted in the table: ")
+def get_data(cursor):
+    data=cursor.execute('''SELECT * FROM quote_tags''')
+    for row in data:
+        print(row)
+
+def calling_all_functions():
+    quotes,authors = get_json_data()
+    cursor = get_sqlite_connection()
+    create_author_table(cursor)
+    create_quote_table(cursor)
+    create_quote_tags_table(cursor)
+    enable_foreign_keys(cursor)
+    insert_values_into_author_table(authors,cursor)
+    insert_values_into_quote_table(quotes,cursor)
+    get_and_insert_list_of_tags_of_each_quote(quotes,cursor)
+    get_data(cursor)
+
+
+calling_all_functions()
